7531b6c2e1a752a046e6bd110bb4e7d0ca3fa041
KumarArnob <kumararnob07@gmail.com>
2021-04-11 18:08:58 +0530

    going to host

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,22 +4,22 @@ const lists = [
   {
     work: "Weather App",
     img: "images/weather.png",
-    desc:"A app that shows desired place's weather info .. "
+    desc: "A app that shows desired place's weather info .. ",
   },
   {
     work: "Traffic Monitor ",
     img: "images/xero.png",
-    desc:"I have developed a Traffic monitor frontend , using NodeJS , expressJs ."
-  },
-  {
-    work: "3N Events",
-    img: "images/3NEvents.png",
-    desc:"An event organize web platform from where user can book parties , reception ceremony ."
+    desc:"I have developed a Traffic monitor frontend , using NodeJS , expressJs .",
   },
   {
     work: "Video Converter",
     img: "images/converter.png",
-    desc:"A unique converter that converts videos in any format like Flash ..."
+    desc:"A unique converter that converts videos in any format like Flash ...",
+  },
+  {
+    work: "3N Events",
+    img: "images/3NEvents.png",
+    desc: "An event organize web platform from where user can book parties .",
   },
 ];
 
@@ -34,34 +34,39 @@ const Project = () => {
                 <h2>My Projects</h2>
                 <span />
               </div>
-              <span className="heading-meta-sub">PORTFOLIO</span>
+              <span className="heading-meta-sub">PROJECTS</span>
               <p>
-               I have worked on several projects using ReactJS , NodeJS . By developing these projects I learned new technologies ,
-               build my problem solving skills .
+                I have worked on several projects using ReactJS , NodeJS . By
+                developing these projects I learned new technologies , build my
+                problem solving skills .
               </p>
             </div>
           </div>
+
           <div className="row">
-                  {lists?.map((opt) => (
-                 <div className="col-sm-6">
+            <div class="col-lg-12">
+                {lists?.map((opt) => (
+              <div class="work-gallery">
+                  <div className="col-lg-6 col-md-6 col-sm-12">
                     <div className="work">
                       <img className="img-fluid" src={opt.img} alt />
                       <div className="desc">
                         <div className="con">
                           <h3>
-                            <a href="#">{opt.work}</a>
+                            {/* <a href="#">{opt.work}</a> */}
+                            {opt.work}
                           </h3>
                           <span>{opt.desc}</span>
                         </div>
                       </div>
                     </div>
-                </div>
-                  ))}
-            
+                  </div>
+              </div>
+                ))}
+            </div>
           </div>
         </div>
       </section>
-      
     </div>
   );
 };
